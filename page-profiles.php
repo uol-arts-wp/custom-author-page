9756bfbf344546310be9dde247e6159eb7e8f346
Steve Honeyman <fvosh@leeds.ac.uk>
2016-05-18 14:37:04 +0100

    Basic changes to structure of html

--- page-profiles.php
+++ page-profiles.php
@@ -4,38 +4,36 @@ Template Name: Author Profiles
 */
 get_header(); ?>
 
-
-
-<div class="profiles">
+  <div class="profiles">
 
   <!--============== basic loop ==============-->
 
-  <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+    <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 
-  	<?php the_content(); ?>
-     
-  <?php endwhile; else : ?>
-  	<p><?php _e( 'Sorry, no posts matched your criteria.' ); ?></p>
-  <?php endif; ?>
+      <?php the_content(); ?>
+       
+    <?php endwhile; else : ?>
+    	<p><?php _e( 'Sorry, no posts matched your criteria.' ); ?></p>
+    <?php endif; ?>
 
-  <!--=============== WP_Query Loop ======================-->
+    <!--=============== WP_Query Loop ======================-->
 
-  <?php $args = array( 'post_type' => 'profile'); 
-  $the_query = new WP_Query( $args ); ?>       
-      
-      <?php if (have_posts () ) : while ( $the_query->have_posts() ) : $the_query->the_post(); ?>
-     
-      <div class="profile-container">   
-        <a href="<?php the_permalink();?>"><?php the_title(); ?><br />
-         <?php the_post_thumbnail(); ?></a>
-      </div>
-         
-      <?php endwhile; else:?>
-  <?php endif; ?>   
-  <?php wp_reset_postdata(); ?>
+        <?php $args = array( 'post_type' => 'profile'); 
+        $the_query = new WP_Query( $args ); ?>       
+        
+        <?php if (have_posts () ) : while ( $the_query->have_posts() ) : $the_query->the_post(); ?>
+       
+          <div class="profile-container">   
+            <a href="<?php the_permalink();?>"><?php the_title(); ?><br />
+             <?php the_post_thumbnail(); ?></a>
+          </div>
+           
+        <?php endwhile; else:?>
+        <?php endif; ?>   
+        <?php wp_reset_postdata(); ?>
 
-</div>
+  </div>
 
-<div class="clearer">&nbsp</div>  
+  <div class="clearer">&nbsp;</div>  
 
 <?php get_footer(); ?>
